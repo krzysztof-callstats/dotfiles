688847bb7277d123fcbd2980c2c71f53e7d5f70f
Krzysztof Dziadowiec <krzysztof.dziadowiec@gmail.com>
2018-08-09 11:55:25 +0200

    Update install_fedora.sh

--- install_fedora.sh
+++ install_fedora.sh
@@ -5,3 +5,12 @@
 
 rm -rf /tmp/repo &>/dev/null
 mkdir -p /tmp/repo
+cd /tmp/repo
+curl -L https://github.com/devligue/dotfiles/archive/master.zip > repo.zip
+sudo yum -y install unzip
+unzip repo.zip
+rm repo.zip
+mv dotfiles* ~/.dotfiles
+cd ~/.dotfiles
+rm -rf /tmp/repo
+bash setup_fedora.sh
