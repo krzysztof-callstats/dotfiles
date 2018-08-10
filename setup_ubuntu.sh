2a5b027bcc6e4bfabc559234ac8f9d65cc3fef77
Krzysztof Dziadowiec <krzysztof.dziadowiec@gmail.com>
2018-08-10 13:35:12 +0200

    Create setup_ubuntu.sh

---
+++ setup_ubuntu.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+source ./lib_sh/echos.sh
+bot "Hi! I'm going to setup this machine. Here I go..."
+
+bot "Configuring git"
+grep 'user = GITHUBUSER' ./homedir/.gitconfig > /dev/null 2>&1
+if [[ $? = 0 ]]; then
+    read -r -p "What is your github.com username? " githubuser
+    read -r -p "What is your first name? " firstname
+    read -r -p "What is your last name? " lastname
+    read -r -p "What is your email? " email
+    fullname="$firstname $lastname"
+    bot "Greatings $fullname, "
+    sed -i "s/GITHUBFULLNAME/$firstname $lastname/" ./homedir/.gitconfig > /dev/null 2>&1
+    sed -i 's/GITHUBEMAIL/'$email'/' ./homedir/.gitconfig
+    sed -i 's/GITHUBUSER/'$githubuser'/' ./homedir/.gitconfig
+fi
+
+bot "Configuring Repository"
+sudo apt-get install software-properties-common
+sudo apt-add-repository ppa:neovim-ppa/stable
+
+bot "Update packages"
+sudo apt-get update
+
+bot "Install python and other necessary tools"
+sudo apt install python-pip
+sudo apt install python3-pip
+pip install --user neovim
+pip3 install --user pipenv neovim flake8
+
+bot "Installing neovim"
+sudo apt-get install neovim
+
+bot "creating symlinks for project dotfiles..."
+pushd homedir > /dev/null 2>&1
+now=$(date +"%Y.%m.%d.%H.%M.%S")
+
+for file in .*; do
+  if [[ $file == "." || $file == ".." ]]; then
+    continue
+  fi
+  # if the file exists:
+  if [[ -e ~/$file ]]; then
+      mkdir -p ~/.dotfiles_backup/$now
+      mv ~/$file ~/.dotfiles_backup/$now/$file
+      echo "backup saved as ~/.dotfiles_backup/$now/$file"
+  fi
+  # symlink might still exist
+  unlink ~/$file > /dev/null 2>&1
+  # create the link
+  ln -s ~/.dotfiles/homedir/$file ~/$file
+  echo -en linking ${file}; ok
+done
+
+popd > /dev/null 2>&1
+
+
+# Configure BASH
+# mkdir -p ~/bin
+# grep 'source ~/.profile' ~/.bashrc || echo "source ~/.profile" >> ~/.bashrc
+
+bot "setting up neovim"
+nvim +PluginInstall +qall
+#sudo reboot
