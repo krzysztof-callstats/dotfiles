02df8b7eb0094de46796911ad673714d942e66b3
Krzysztof Dziadowiec <krzysztof.dziadowiec@gmail.com>
2018-08-13 23:01:51 +0200

    Update cd_extensions.sh

--- lib_sh/cd_extensions.sh
+++ lib_sh/cd_extensions.sh
@@ -5,18 +5,18 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 CD_HISTORY_FILE="${SCRIPT_DIR}/.cd_history"
-if [ ! -e "$CD_HISTORY_FILE" ] ; then
+if [[ ! -e "$CD_HISTORY_FILE" ]] ; then
   touch "$CD_HISTORY_FILE"
 fi
 
 CD_FAV_FILE="${SCRIPT_DIR}/.cd_favorite"
-if [ ! -e "$CD_FAV_FILE" ] ; then
+if [[ ! -e "$CD_FAV_FILE" ]] ; then
   touch "$CD_FAV_FILE"
 fi
 
 function c () {
   LAST_HISTORY_ENTRY=`awk '/./{line=$0} END{print line}' "$CD_HISTORY_FILE"`
-  if [ $PWD != $LAST_HISTORY_ENTRY ] ; then
+  if [[ $PWD != $LAST_HISTORY_ENTRY ]] ; then
     echo $PWD >> $CD_HISTORY_FILE
   fi
   builtin cd "$@"
@@ -27,7 +27,7 @@ function cd {
 }
 
 function cb () {
-  if ! [ -z "`cat $CD_HISTORY_FILE`" ]
+  if ! [[ -z "`cat $CD_HISTORY_FILE`" ]]
     then
       PREV_DIR=`cat $CD_HISTORY_FILE | tail -1`;
       c "$PREV_DIR";
@@ -37,7 +37,7 @@ function cb () {
 }
 
 function cbb () {
-  if ! [ -z "`cat $CD_HISTORY_FILE`" ]
+  if ! [[ -z "`cat $CD_HISTORY_FILE`" ]]
     then
       tac $CD_HISTORY_FILE | tail -10 | awk '{printf("%d %s\n", NR, $0)}';
       echo "Which one?";
@@ -50,18 +50,20 @@ function cbb () {
 }
 
 function cf () {
-  if [ $# -eq 0 ]
+  if [[ $# -eq 0 ]]
     then
-      if ! [ -z "`cat $CD_FAV_FILE`" ]
+      if ! [[ -z "`cat $CD_FAV_FILE`" ]]
         then
           FAV_DIR=`cat $CD_FAV_FILE`;
           c "$FAV_DIR"
       else
-        echo "No favorite directory set. Use:"
+        echo "No favorite directory set. To set it up use:"
         echo "cf /your/favorite/dir"
       fi
   else
     echo "$(readlink -f $@)" > $CD_FAV_FILE;
     echo "Your favorite directory is now:" $(readlink -f $@)
+    echo "To cd to your favorite directory, use: `cf`"
+    echo "To change saved favorite directory, use: `cf /your/new/favorite/dir`"
   fi
 }
