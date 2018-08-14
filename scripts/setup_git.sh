b3f53bfccd40183a9d5caf1bb0e34d8d5bc15dc2
Krzysztof Dziadowiec <krzysztof.dziadowiec@gmail.com>
2018-08-14 23:26:22 +0200

    Create setup_git.sh

---
+++ scripts/setup_git.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+bot "Configuring git. I'm gonna need your help..."
+
+grep 'user = GITHUBUSER' ./homedir/.gitconfig > /dev/null 2>&1
+if [[ $? = 0 ]]; then
+    read -r -p "What is your github.com username? " githubuser
+    read -r -p "What is your first name? " firstname
+    read -r -p "What is your last name? " lastname
+    read -r -p "What is your email? " email
+    fullname="$firstname $lastname"
+    bot "Greatings $fullname, "
+    sed -i "s/GITHUBFULLNAME/$firstname $lastname/" ./homedir/.gitconfig > /dev/null 2>&1
+    sed -i 's/GITHUBEMAIL/'$email'/' ./homedir/.gitconfig
+    sed -i 's/GITHUBUSER/'$githubuser'/' ./homedir/.gitconfig
+fi
