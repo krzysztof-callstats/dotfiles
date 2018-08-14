e3b81c9a7bb50cdbde3b579e5d97ae3ca75fe629
Krzysztof Dziadowiec <krzysztof.dziadowiec@gmail.com>
2018-08-14 23:55:44 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
 
 # Repo download and unzip
-./scripts/prepare_repo.sh
+DNF_CMD=$(which dnf)
+YUM_CMD=$(which yum)
+APT_CMD=$(which apt)
+
+if [[ ! -z $DNF_CMD ]]; then
+  sudo dnf install unzip
+elif [[ ! -z $YUM_CMD ]]; then
+  sudo yum install unzip
+elif [[ ! -z $APT_GET_CMD ]]; then
+  sudo apt install unzip
+else
+  echo "error can't install package unzip"
+  exit 1;
+fi
+
+rm -rf /tmp/repo &>/dev/null
+mkdir -p /tmp/repo
+cd /tmp/repo
+curl -L https://github.com/devligue/dotfiles/archive/master.zip > repo.zip
+unzip repo.zip
+rm repo.zip
+cd dotfiles*
+mkdir -p ~/.dotfiles
+cp -a . ~/.dotfiles
+cd ~/.dotfiles
+rm -rf /tmp/repo
 
 source ./lib_sh/echos.sh
 bot "Hi! I'm going to setup this machine. Here I go..."
