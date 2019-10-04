55a8b30760fea0f11bb661ba63cfd63520dbda84
Krzysztof Dziadowiec <45563326+krzysztof-callstats@users.noreply.github.com>
2019-10-04 16:48:00 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,12 +5,16 @@ DNF_CMD=$(which dnf)
 YUM_CMD=$(which yum)
 APT_CMD=$(which apt)
 
+UNZIP_CMD=${which unzip}
+
 if [[ ! -z $DNF_CMD ]]; then
   sudo dnf install unzip
 elif [[ ! -z $YUM_CMD ]]; then
   sudo yum install unzip
 elif [[ ! -z $APT_CMD ]]; then
   sudo apt install unzip
+elif [[ ! -z $UNZIP_CMD ]]; then
+  echo "unzip already installed!"
 else
   echo "error can't install package unzip"
   exit 1;
